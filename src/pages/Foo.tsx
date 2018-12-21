d891a3b82a8a74a30229eee7a693218911b8637f
Tomáš Holas <tomas.holas@gmail.com>
2018-12-21 19:54:30 +0100

    cleanup

--- src/pages/Foo.tsx
+++ src/pages/Foo.tsx
@@ -2,13 +2,11 @@ import * as React from 'react'
 import Button from '@material-ui/core/Button'
 export class Foo extends React.Component {
   render() {
-    return (
-      <div>
-        <h1 className="text-green">FOO23</h1>
-        <Button color="primary" variant="contained">
-          haha
+    return <div>
+      <h1 className="text-green">FOO23</h1>
+      <Button color="primary" variant="contained">
+        haha
         </Button>
-      </div>
-    )
+    </div>
   }
 }
